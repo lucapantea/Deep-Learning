18064c907d65a6a480f7932d54780b2a21724273
Luca   Pantea <lcur0630@test3.irc.sara.nl.irc.sara.nl>
2022-11-28 22:21:02 +0100

    Refactoring of results

--- assignment2/part2/custom_clipzs_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash  
-
-# Output file 
-output=clipzs_CLIP-B32-custom.txt  
-
-python clipzs.py --root "../part1/data" --prompt_template "The primary colour mostly present is {}" --class_names red blue green --visualize_predictions >> $output
-python clipzs.py --root "../part1/data" --prompt_template "The object is of {} origin" --class_names "human-made" "nature" --visualize_predictions >> $output
